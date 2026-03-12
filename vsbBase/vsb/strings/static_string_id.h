f4f1fe60a2889455b7d4b40c5418ffd56377528a
Alexander <vadersb@gmail.com>
2026-03-12 14:19:31 +0300

    Update static_string_id.h

--- vsbBase/vsb/strings/static_string_id.h
+++ vsbBase/vsb/strings/static_string_id.h
@@ -32,6 +32,12 @@ namespace vsb
 		}
 
 
+		operator std::string_view() const noexcept
+		{
+			return m_String;
+		}
+
+
 		bool operator ==(const StringID& stringID) const
 		{
 			const StringID id = *this;
@@ -47,3 +53,48 @@ namespace vsb
 		mutable StringID m_cachedID {};
 	};
 }
+
+
+
+//TO ADD LATER:
+// template<std::size_t N>
+// struct FixedString
+// {
+// 	char data[N]{};
+// 	std::size_t len{0};
+//
+// 	consteval FixedString() = default;
+//
+// 	consteval FixedString(const char (&str)[N])
+// 	{
+// 		for (std::size_t i = 0; i < N; ++i)
+// 			data[i] = str[i];
+// 		len = N - 1;
+// 	}
+//
+// 	consteval operator std::string_view() const { return {data, len}; }
+//
+// 	template<std::size_t M>
+// 	consteval auto operator+(const FixedString<M>& other) const
+// 	{
+// 		FixedString<N + M - 1> result;
+// 		for (std::size_t i = 0; i < len; ++i)
+// 			result.data[i] = data[i];
+// 		for (std::size_t i = 0; i < other.len; ++i)
+// 			result.data[len + i] = other.data[i];
+// 		result.data[len + other.len] = '\0';
+// 		result.len = len + other.len;
+// 		return result;
+// 	}
+// };
+//
+// template<std::size_t N>
+// FixedString(const char (&)[N]) -> FixedString<N>;
+
+
+//USAGE:
+// constexpr auto prefix = FixedString("System.");
+// constexpr auto name   = FixedString("Renderer");
+// constexpr auto full   = prefix + name; // "System.Renderer"
+//
+// static constexpr StaticStringID id{static_cast<std::string_view>(full)};
